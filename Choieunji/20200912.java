3bc4581435f38424c51df3bf5aa0d9e5bbf97fe2
choieunji <62586967+choieunii@users.noreply.github.com>
2020-09-12 12:07:00 +0900

    Programmars/ 연습문제/ 같은 숫자는 싫어

---
+++ Choieunji/20200912.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int [] answer = {};
+        ArrayList list =new ArrayList<Integer>();
+        list.add(arr[0]);
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i-1]!=arr[i])
+            {
+               list.add(arr[i]);
+            }
+        }
+        answer= new int[list.size()];
+        for(int i=0;i<list.size();i++)
+        {
+            answer[i]=(int)list.get(i);
+        }
+        return answer;
+    }
+}
